b92caac4a834f3038778ac5ae4a44b68489d18a4
keenOnFrontEnd <73851144+keenOnFrontEnd@users.noreply.github.com>
2020-11-05 21:31:31 +0100

    Updates

---
+++ precache-manifest.d9b3601844fdd6752f64aaec37bade8e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22406428f57842a15f756169f695e742",
+    "url": "/MyFirstProject/index.html"
+  },
+  {
+    "revision": "d13a7f18a1163a3e0a49",
+    "url": "/MyFirstProject/static/css/3.cc5c04c1.chunk.css"
+  },
+  {
+    "revision": "26ce4fd966023a90a094",
+    "url": "/MyFirstProject/static/css/4.742f2e6c.chunk.css"
+  },
+  {
+    "revision": "1c031e6a4230d1a1cd99",
+    "url": "/MyFirstProject/static/css/main.3acfe329.chunk.css"
+  },
+  {
+    "revision": "1376261dbffe3205c6e1",
+    "url": "/MyFirstProject/static/js/2.bf6a4ab6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/MyFirstProject/static/js/2.bf6a4ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13a7f18a1163a3e0a49",
+    "url": "/MyFirstProject/static/js/3.7f0c99b6.chunk.js"
+  },
+  {
+    "revision": "26ce4fd966023a90a094",
+    "url": "/MyFirstProject/static/js/4.3d4d1dd4.chunk.js"
+  },
+  {
+    "revision": "1c031e6a4230d1a1cd99",
+    "url": "/MyFirstProject/static/js/main.d76c583e.chunk.js"
+  },
+  {
+    "revision": "017459ac486abefae2fe",
+    "url": "/MyFirstProject/static/js/runtime-main.f2853cd2.js"
+  },
+  {
+    "revision": "f4d7875d836558dc64082fcc6d7d0b6c",
+    "url": "/MyFirstProject/static/media/preLoad.f4d7875d.svg"
+  },
+  {
+    "revision": "2ea7295b1220e3eada951ffa8b93beaa",
+    "url": "/MyFirstProject/static/media/twitter-icon.2ea7295b.png"
+  },
+  {
+    "revision": "0f8e789e159dcc08df61455201923173",
+    "url": "/MyFirstProject/static/media/user_icon.0f8e789e.jpg"
+  },
+  {
+    "revision": "5032d8621d7c03873a96ef2af9f8ce05",
+    "url": "/MyFirstProject/static/media/vk-icon.5032d862.png"
+  }
+]);
